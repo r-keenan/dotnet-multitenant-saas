011d6cf6bd6aa728b264189eff7e007f492d97f1
r-keenan <ross.keenan@protonmail.com>
2025-02-26 12:14:59 -0600

    Added AppRole, RoleClaim, and User. removed unused class file.

---
+++ src/Infrastructure/Identity/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Identity.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public bool IsActive { get; set; }
+    public string RefreshToken { get; set; }
+    public DateTime RefreshTokenExpirationTime { get; set; }
+}
