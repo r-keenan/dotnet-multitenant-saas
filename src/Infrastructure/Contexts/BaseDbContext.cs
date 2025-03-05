6dd19f5a7144c1610522afeaa7cdb18fbce3a160
r-keenan <ross.keenan@protonmail.comm>
2025-03-05 12:59:37 -0600

    updateded basedbcontext to run migration if connection string is not null

--- src/Infrastructure/Contexts/BaseDbContext.cs
+++ src/Infrastructure/Contexts/BaseDbContext.cs
@@ -4,13 +4,30 @@ using Infrastructure.Identity.Models;
 using Infrastructure.Tenancy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace Infrastructure.Contexts;
 
 public abstract class BaseDbContext : MultiTenantIdentityDbContext<ApplicationUser, ApplicationRole, string,
     IdentityUserClaim<string>, IdentityUserRole<string>, IdentityUserLogin<string>, ApplicationRoleClaim, IdentityUserToken<string>>
 {
+    private new ABCSchoolTenantInfo TenantInfo { get; set; }
+
     protected BaseDbContext(IMultiTenantContextAccessor<ABCSchoolTenantInfo> tenantContextAccessor, DbContextOptions options) : base(tenantContextAccessor, options)
     {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+
+        // this will run migrations across all schemas
+        if (!string.IsNullOrEmpty(TenantInfo?.ConnectionString))
+        {
+            optionsBuilder.UseNpgsql(TenantInfo.ConnectionString, opts =>
+            {
+                opts.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+            });
+        }
     }
 }
